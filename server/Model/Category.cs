d6b5c1db65f192645fc7a38928777c32a36930a0
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-03-23 17:00:20 +0700

    Auto-sync: file changes

--- server/Model/Category.cs
+++ server/Model/Category.cs
@@ -1,11 +1,14 @@
-namespace ITServiceDesk.Api.Models
+namespace ITServiceDesk.Api.Models;
+
+using System.Text.Json.Serialization;
+
 {
     public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-
-        public ICollection<Ticket> Tickets { get; set; }
-    }
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    [JsonIgnore]   // 👈 THÊM DÒNG NÀY
+    public ICollection<Ticket> Tickets { get; set; }
+}
 }
