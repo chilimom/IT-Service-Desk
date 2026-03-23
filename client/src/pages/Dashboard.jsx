184d4b7d030c2edcc843c30d9095f67bd62654a6
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-03-23 11:00:05 +0700

    Auto-sync: file changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -131,30 +131,9 @@ function Dashboard() {
             </div>
           </div>
           <div className="chart-container">
-  {/* <ResponsiveContainer width="100%" height={320}>
-    <PieChart>
-      <Pie
-  data={chartData}
-  dataKey="value"
-  nameKey="name"
-  outerRadius="90%"
-  cx="50%"
-  cy="45%"  
-  label ={({ percent }) =>
-        `${(percent * 100).toFixed(0)}%`
-      }
->
-  {chartData.map((entry, index) => (
-    <Cell key={index} fill={entry.color} />
-  ))}
-</Pie>
-      <Tooltip />
-      <Legend verticalAlign="bottom" height={40} />
-    </PieChart>
-  </ResponsiveContainer> */}
   <ResponsiveContainer width="100%" height={320}>
   <PieChart>
-    {/* <Pie
+    <Pie
       data={chartData}
       dataKey="value"
       nameKey="name"
@@ -168,24 +147,7 @@ function Dashboard() {
       {chartData.map((entry, index) => (
         <Cell key={index} fill={entry.color} />
       ))}
-    </Pie> */}
-    <Pie
-            data={chartData}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            innerRadius="60%"
-            outerRadius="90%"
-            paddingAngle={3}
-            cornerRadius={10}
-            onMouseEnter={(_, index) => setActiveIndex(index)}
-            onMouseLeave={() => setActiveIndex(null)}
-          >
-            {chartData.map((entry, index) => (
-              <Cell key={index} fill={entry.color} />
-            ))}
-          </Pie>
+    </Pie>
 
     {/* TEXT Ở GIỮA */}
     <text
