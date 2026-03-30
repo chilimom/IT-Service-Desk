9d1d948282b8f25ea973481c29a51d2bf551d1fa
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-03-30 13:16:27 +0700

    Auto-sync: file changes

--- server/Program.cs
+++ server/Program.cs
@@ -3,6 +3,8 @@ using ITServiceDesk.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls(
+    "http://localhost:5016", "http://10.192.72.45:5016");
 
 const string FrontendCorsPolicy = "FrontendCorsPolicy";
 
@@ -15,34 +17,34 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:3001",
-                "https://localhost:3001",
-                "http://localhost:5173",
-                "https://localhost:5173",
-                "http://localhost:5174",
-                "https://localhost:5174",
-                "http://localhost:5175",
-                "https://localhost:5175",
-                "http://localhost:8081",
+                // "http://localhost:3001",
+                // "https://localhost:3001",
+                // "http://localhost:5173",
+                // "https://localhost:5173",
+                // "http://localhost:5174",
+                // "https://localhost:5174",
+                // "http://localhost:5175",
+                // "https://localhost:5175",
+                // "http://localhost:8081",
                 "https://localhost:8081",
-                "http://127.0.0.1:5174",
-                "https://127.0.0.1:5174",
-                "http://127.0.0.1:5175",
-                "https://127.0.0.1:5175",
-                "http://127.0.0.1:8081",
+                // "http://127.0.0.1:5174",
+                // "https://127.0.0.1:5174",
+                // "http://127.0.0.1:5175",
+                // "https://127.0.0.1:5175",
+                // "http://127.0.0.1:8081",
                 "https://127.0.0.1:8081",
-                "http://10.192.72.45:5173",
-                "https://10.192.72.45:5173",
-                "http://10.192.72.45:5174",
-                "https://10.192.72.45:5174",
-                "http://10.192.72.45:5175",
-                "https://10.192.72.45:5175",
-                "http://10.192.72.45:8081",
-                "https://10.192.72.45:8081",
-                "http://localhost:3002",
-                "https://localhost:3002",
-                "http://10.192.72.45:3002",
-                "https://10.192.72.45:3002")
+                // "http://10.192.72.45:5173",
+                // "https://10.192.72.45:5173",
+                // "http://10.192.72.45:5174",
+                // "https://10.192.72.45:5174",
+                // "http://10.192.72.45:5175",
+                // "https://10.192.72.45:5175",
+                "http://10.192.72.45:8081")
+            // "https://10.192.72.45:8081",
+            // "http://localhost:3002",
+            // "https://localhost:3002",
+            // "http://10.192.72.45:3002",
+            // "https://10.192.72.45:3002")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
