91a0d944681ed0f063136bc9df2b49f3db099196
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-04-08 10:45:44 +0700

    Auto-sync: file changes

--- server/Program.cs
+++ server/Program.cs
@@ -9,7 +9,7 @@ builder.Logging.AddConsole();
 
 // ===== CONFIG PORT BACKEND =====
 builder.WebHost.UseUrls(
-    "http://localhost:5018", "http://10.192.72.45:5016");
+    "http://localhost:5016", "http://10.192.72.45:5016");
 
 const string FrontendCorsPolicy = "FrontendCorsPolicy";
 
@@ -24,9 +24,9 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:8080",
+                "http://localhost:8081",
 
-                "http://127.0.0.1:8080",
+                "http://127.0.0.1:8081",
 
                 "http://10.192.72.45:8081")
 
--- server/obj/Release/net8.0/server.AssemblyInfo.cs
+++ server/obj/Release/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6b4242d265a53a9b7389a5b154f724f0a864b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26a36a41c0f421dc3724a01f0d53edb08208eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
