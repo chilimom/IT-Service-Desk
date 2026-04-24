1d28fa2a0822c9ecd50c3366cf719e617dea5e80
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-04-24 07:32:26 +0700

    Auto-sync: file changes

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd4ebcd869ed2245594192562b16e2701137d1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa189866333172216c138e679332c1ba6ce9d57")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
